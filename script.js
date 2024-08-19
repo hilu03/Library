a4e3e2d0316b0edb8127cabad07dea12e352cd5c
Luong Tran <tranthihienluong2003@gmail.com>
2024-08-19 21:33:42 +0700

    Display book on the page

---
+++ script.js
@@ -0,0 +1,43 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+const book1 = new Book("Name of the Book", "Name of author", 200, false);
+const book2 = new Book("Name of the Book", "Name of author", 200, false);
+const book3 = new Book("Name of the Book", "Name of author", 200, false);
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+
+function displayBook() {
+  const container = document.querySelector(".book-container");
+  let html = "";
+  myLibrary.forEach(book => {
+    html +=
+      ` <div class="card">
+          <div class="icon-container">
+            <img src="book_icon.png" class="icon">
+          </div>
+          <div class="title">${book.title}</div>
+          <div class="author">Author: ${book.author}</div>
+          <div class="last-row">
+            <div class="pages">${book.pages} pages</div>
+            <div class="read">${book.read? "Read": "Unread"}</div>  
+          </div>
+        </div>
+      `;
+  });
+  container.innerHTML = html;
+}
+
+displayBook();

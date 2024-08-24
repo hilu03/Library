5854c4bab054a6f5936203a0048876abd78f8f8f
Luong Tran <tranthihienluong2003@gmail.com>
2024-08-24 19:55:21 +0700

    Change to use Library class

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const myLibrary = [];
-
 class Book {
   constructor(title, author, pages, status) {
     this.title = title;
@@ -9,75 +7,82 @@ class Book {
   }
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+class Library {
+  books = [];
+
+  constructor() {
+    const book1 = new Book("Kane and Abel", "Jeffrey Archer", 695, "read");
+    const book2 = new Book("Anne of Green Gables", "Lucy Maud Montgomery", 320, "read");
+    const book3 = new Book("Harry Potter and the Sorcerer's Stone", "J. K. Rowling", 309, "unread");
+    
+    this.addBook(book1);
+    this.addBook(book2);
+    this.addBook(book3);
+  }
+
+  addBook(book) {
+    this.books.push(book);
+  }  
+
+  displayBook() {
+    const container = document.querySelector(".book-container");
+    let html = "";
+    this.books.forEach((book, index) => {
+      html +=
+        ` <div class="card ${book.status === 'read'? 'green-card': 'orange-card'}" data-id="${index}">
+            <div class="icon-container">
+              <img src="book_icon.png" class="icon">
+            </div>
+            <div class="title">${book.title}</div>
+            <div class="author">Author: <em>${book.author}</em></div>
+            <div class="last-row">
+              <div class="pages">${book.pages} pages</div>
+              <button class="${book.status}" data-id="${index}">${book.status}</button>  
+            </div>
+            <button class="remove" data-id="${index}">Remove</button>
+          </div>
+        `;
+    });
+    container.innerHTML = html;
+  
+    const removeButtons = document.querySelectorAll(".remove");
+    removeButtons.forEach(button => {
+      button.addEventListener("click", () => {
+        const index = Number(button.dataset.id);
+        this.books.splice(index, 1);
+        this.displayBook();
+      });
+    });
+  
+    const statusButtons = document.querySelectorAll(".last-row button");
+    statusButtons.forEach((button, id) => {
+      button.addEventListener("click", () => {
+        const status = button.textContent;
+        const index = Number(button.dataset.id);
+        const card = document.querySelector(`.card[data-id="${id}"]`);
+        if (status === "Read") {
+          button.classList.remove("read");
+          button.classList.add("unread");
+          card.classList.remove("green-card");
+          card.classList.add("orange-card");
+          button.textContent = "Unread";
+          this.books[index].status = "unread";
+        }
+        else {
+          button.classList.remove("unread");
+          button.classList.add("read");
+          card.classList.remove("orange-card");
+          card.classList.add("green-card");
+          button.textContent = "Read";
+          this.books[index].status = "read";
+        }
+      });
+    });
+  }
 }
 
-const book1 = new Book("Kane and Abel", "Jeffrey Archer", 695, "read");
-const book2 = new Book("Anne of Green Gables", "Lucy Maud Montgomery", 320, "read");
-const book3 = new Book("Harry Potter and the Sorcerer's Stone", "J. K. Rowling", 309, "unread");
-
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
-
-function displayBook() {
-  const container = document.querySelector(".book-container");
-  let html = "";
-  myLibrary.forEach((book, index) => {
-    html +=
-      ` <div class="card ${book.status === 'read'? 'green-card': 'orange-card'}" data-id="${index}">
-          <div class="icon-container">
-            <img src="book_icon.png" class="icon">
-          </div>
-          <div class="title">${book.title}</div>
-          <div class="author">Author: <em>${book.author}</em></div>
-          <div class="last-row">
-            <div class="pages">${book.pages} pages</div>
-            <button class="${book.status}" data-id="${index}">${book.status}</button>  
-          </div>
-          <button class="remove" data-id="${index}">Remove</button>
-        </div>
-      `;
-  });
-  container.innerHTML = html;
-
-  const removeButtons = document.querySelectorAll(".remove");
-  removeButtons.forEach(button => {
-    button.addEventListener("click", () => {
-      const index = Number(button.dataset.id);
-      myLibrary.splice(index, 1);
-      displayBook();
-    });
-  });
-
-  const statusButtons = document.querySelectorAll(".last-row button");
-  statusButtons.forEach((button, id) => {
-    button.addEventListener("click", () => {
-      const status = button.textContent;
-      const index = Number(button.dataset.id);
-      const card = document.querySelector(`.card[data-id="${id}"]`);
-      if (status === "Read") {
-        button.classList.remove("read");
-        button.classList.add("unread");
-        card.classList.remove("green-card");
-        card.classList.add("orange-card");
-        button.textContent = "Unread";
-        myLibrary[index].status = "unread";
-      }
-      else {
-        button.classList.remove("unread");
-        button.classList.add("read");
-        card.classList.remove("orange-card");
-        card.classList.add("green-card");
-        button.textContent = "Read";
-        myLibrary[index].status = "read";
-      }
-    });
-  });
-}
-
-displayBook();
+const library = new Library();
+library.displayBook();
 
 const dialog = document.querySelector("dialog");
 const showForm = document.querySelector(".show-form");
@@ -93,9 +98,9 @@ addButton.addEventListener("click", (e) => {
   const page = document.querySelector("#page").value;
   const status = document.querySelector("input[name=status]:checked").value;
   const book = new Book(title, author, page, status);
-  addBookToLibrary(book);
+  library.addBook(book);
   e.preventDefault();
   dialog.close();
-  displayBook();
+  library.displayBook();
 });
 
